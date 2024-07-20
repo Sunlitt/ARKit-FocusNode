a1abd7783720081ff33da0227889f58542f03fa9
Nicolas Mariniello <nmariniello.dev@icloud.com>
2024-07-20 17:47:30 +0200

    Cleanup + raised requirements

--- Package.swift
+++ Package.swift
@@ -1,23 +1,19 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-  name: "FocusNode",
-  platforms: [.iOS("11.3")],
-  products: [
-    .library(name: "FocusNode", targets: ["FocusNode"]),
-  ],
-  dependencies: [
-//  .package(path: "../ARKit-SmartHitTest")
-    .package(
-      url: "https://github.com/maxxfrazer/ARKit-SmartHitTest",
-      .upToNextMajor(from: "2.0.0")
-    )
-  ],
-  targets: [
-    .target(name: "FocusNode", dependencies: ["SmartHitTest"])
-  ],
-  swiftLanguageVersions: [.v5]
+    name: "FocusNode",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(name: "FocusNode", targets: ["FocusNode"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/Sunlitt/ARKit-SmartHitTest.git",
+            .upToNextMajor(from: "2.0.0")
+        )
+    ],
+    targets: [.target(name: "FocusNode", dependencies: ["SmartHitTest"])]
 )
